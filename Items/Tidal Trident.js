0f5032a8ef104716c4b6199c308b10c386a8af98
Bruno Lambert <brunofsclambert@gmail.com>
2024-06-03 03:18:43 -0300

    Tidal Trident

---
+++ Items/Tidal Trident.js
@@ -0,0 +1,33 @@
+const applyDamageEffect = async (isEmpowered) => {
+    const effectName = isEmpowered ? 'Enwater (Empowered)' : 'Enwater (Normal)';
+    const uuid = item.actor.uuid;
+    const pb = item.actor.system.attributes.prof;
+
+    const hasEffectApplied = await game.dfreds.effectInterface.hasEffectApplied(effectName, uuid);
+
+    if (!hasEffectApplied) {
+        const effectData = game.dfreds.effectInterface.findEffectByName(effectName).data.toObject();
+        effectData.duration.seconds = pb * 6;
+        game.dfreds.effectInterface.addEffectWith({ effectData, uuid });
+    }
+}
+
+let enwaterDialog = new Dialog({
+    title: "Trident of Sahagin Prince",
+    content: `
+        <p>Your are about to use Enwater feature from the Trident of Sahagin Prince</p>
+        <p>Now, if there is a water source as described at the item you need to tell me</p><br />
+    `,
+    buttons: {
+        yes: {
+            label: "Yes, there is.",
+            callback: () => applyDamageEffect(true)
+        },
+        no: {
+            label: "No, it is dry here",
+            callback: () => applyDamageEffect(false)
+        },
+    }
+});
+
+enwaterDialog.render(true);
