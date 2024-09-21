920451ae1565273cc60eef20a926f01c8aa72268
Bruno Lambert <brunofsclambert@gmail.com>
2024-09-20 22:10:06 -0300

    New features added on level 6

---
+++ Arcanist/Summoner/Radiant Aegis.js
@@ -0,0 +1,30 @@
+const findingTokens = canvas.tokens.ownedTokens.filter(token => token.document.name.toLowerCase().search(/ifrit|garuda|titan/gm) >= 0);
+console.log(findingTokens)
+const atherialCompanionToken = findingTokens[0]?.document;
+
+const source = token;
+const destiny = atherialCompanionToken;
+
+new Sequence()
+    .effect()
+    .file("jb2a.particle_burst.01.star")
+    .atLocation(source)
+    .scale(0.4)
+    .randomRotation()
+    .wait(900)
+    .animation()
+    .on(source)
+    .teleportTo(destiny)
+    .play()
+
+new Sequence()
+    .effect()
+    .file("jb2a.particle_burst.01.circle")
+    .atLocation(destiny)
+    .scale(0.4)
+    .randomRotation()
+    .wait(900)
+    .animation()
+    .on(destiny)
+    .teleportTo(source)
+    .play()
